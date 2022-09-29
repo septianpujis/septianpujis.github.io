f1b74c2bb1cc77782d35f889183d52b90b7e5432
Septian Puji <septian.puji@commsult.id>
2022-09-29 09:44:47 +0700

    change bootstrap to cdn

--- portoSepti/lang_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(!isset($_GET['lang'])){
-        $_GET['lang'] = "en";
-    }
-    $json = file_get_contents("assets/".$_GET['lang']."_dict.json"); ;
-    $text = json_decode($json,true);
-?>
