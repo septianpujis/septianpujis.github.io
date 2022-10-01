29371417c219c781c904a4cc797ba7878d86a16f
septianpujis <septianpujipuji@gmail.com>
2022-10-01 21:19:35 +0700

    finalized language system with js

--- assets/lang/lang_parser.js
+++ assets/lang/lang_parser.js
@@ -1,3 +1,21 @@
-fetch('./assets/lang/en.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+function setLang(lang) {
+    var xmlhttp = new XMLHttpRequest();
+    var obj;
+    var url = lang == "en" ? "./assets/lang/en.json" : "./assets/lang/id.json";
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            getLang(myArr)
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+
+    function getLang(arr) {
+        obj = arr;
+        console.log(obj.about);
+    }
+}
+
+setLang("en")
